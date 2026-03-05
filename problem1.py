310fa6f995d350ed123b739d5d23865d1dba7365
Calvin Dantis <cavlon3006@gmail.com>
2026-03-05 16:27:17 +0000

    Combined Plots

--- problem1.py
+++ problem1.py
@@ -6,8 +6,8 @@ np.set_printoptions(suppress=True)
 # Load dataset, converting data to meters and removing the offset
 data = pygmt.grd2xyz("dataset/dataset/heightmaps/ldem_4_uint.tif+s0.5+o-10000", output_type='numpy')
 
-# Move the data coordinates to the corners of the pixels to line up the longitudes
-data[:, 0] = data[:, 0]-0.5
+# Move the data coordinates to the corners of the pixels to line up the longitudes, also recenter 0 degrees longitude
+data[:, 0] = (data[:, 0]-720.5) % 1440
 data[:, 1] = data[:, 1]-0.5
 
 # Repeat the data at 0 longitude so the polar representation wraps correctly
@@ -31,30 +31,27 @@ fig = pygmt.Figure()
 
 pygmt.makecpt(cmap="magma", series=[z_min, z_max])
 
-perspective = True
-if perspective:
+with fig.shift_origin(xshift="3c", yshift="14c"):
+    fig.grdimage(
+        grid=grid,
+        region="0/360/-90/-60",
+        projection="S0/-90/12c",
+        cmap=True,
+        frame="afg",
+    )
+
+    fig.colorbar(position="jML+o-3c/0c+w12c/0.5c", frame=['x+lElevation', 'y+lm'])
+
+with fig.shift_origin(xshift="w+4c", yshift="-2c"):
     fig.grdview(
         grid=grid,
         region="0/360/-90/-60",
         perspective=[135, 30],
-        projection="S0/-90/24c",
+        projection="S0/-90/16c",
         zscale="0.00005c",
         surftype="s",
         cmap=True,
         frame="afg"
     )
 
-    fig.colorbar(position="jTL+o-3c/0c+w12c/0.5c", frame=['x+lElevation', 'y+lm'])
-
-else:
-    fig.grdimage(
-        grid=grid,
-        region="0/360/-90/-60",
-        projection="S0/-90/24c",
-        cmap=True,
-        frame="afg",
-    )
-
-    fig.colorbar(position=("-5c", "2c"), width="0.5c", length="20c", frame=['x+lElevation', 'y+lm'])
-
-fig.show()
+fig.show(resize="+m1c")
